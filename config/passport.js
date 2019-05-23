87fda55ec248c2ede4367253fb283407896c32f2
Albert Chung <chung972@gmail.com>
2019-05-23 13:39:06 -0500

    adds callback code

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,26 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const User = require("../models/user");
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+}, (accessToken, refreshToken, profile, cb) => {
+    User.findOne({ "googleId": profile.id }, (err, user) => {
+        if (err) return cb(err);
+        if (user) {
+            return cb(null, user);
+        } else {
+            var newUser = new User({
+                name: profile.displayName,
+                googleId: profile.id
+            });
+        }
+        newUser.save(err => {
+            if(err) return cb(err);
+            return cb(null, newUser);
+        });
+    });
+}
+));
