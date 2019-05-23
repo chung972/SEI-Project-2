f8f21b65ba42424e1180795f03b50c295a6c2a9e
Albert Chung <chung972@gmail.com>
2019-05-23 14:02:49 -0500

    adds Google OAuth Login, Logout, and callback routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require("passport");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  // TODO: have index.ejs be a landing page; look at
+  // wireframes for more details
+});
+
+// Google OAuth LOGIN route
+router.get("/auth/google", passport.authenticate(
+  "google",
+  {scope: ["profile"]}
+));
+
+// Google OAuth CALLBACK route
+router.get("/oauth2callback", passport.authenticate(
+  "google",
+  {
+    successRedirect: "/",
+    failureRedirect: "/"
+    // TODO: specify what page we want to redirect to after cb executes
+    // currently redirecting back to index
+  }
+));
+
+router.get("/logout", (req,res)=>{
+  req.logout();
+  res.redirect("/");
+  // TODO: again, we'll want to specify what PATH (i.e. not relative
+  // to the view dir) we want to redirect to
 });
 
 module.exports = router;
