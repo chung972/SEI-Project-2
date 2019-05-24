ef432f2979e255e730695dc9d5e8eed038a6c65b
Albert Chung <chung972@gmail.com>
2019-05-24 09:08:14 -0500

    reorganizes line placement of code

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 // mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 // the above line is what we SHOULD use. configure DATABASE_URL in the .env file;
-
+const devConnectionString = "mongodb://localhost/users";
 // TODO: remember to change the GOOGLE_CALLBACK url once you're deployed on heroku!
+const db = mongoose.connection;
+mongoose.connect(process.env.DATABASE_URL || devConnectionString, { useNewUrlParser: true });
 
-mongoose.connect("mongodb://localhost/users", { useNewUrlParser: true });
-var db = mongoose.connection;
 db.on("connected", function(){
     console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
