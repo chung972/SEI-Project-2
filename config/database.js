20e349bb1756ba2b4b5ac3a3ab9e1fbdceaa5d9a
Albert Chung <chung972@gmail.com>
2019-05-24 13:30:22 -0500

    deletes unnecessary comments

--- config/database.js
+++ config/database.js
@@ -1,22 +1,12 @@
 const mongoose = require("mongoose");
-// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
-// the above line is what we SHOULD use. configure DATABASE_URL in the .env file;
 const devConnectionString = "mongodb://localhost/users";
-// TODO: remember to change the GOOGLE_CALLBACK url once you're deployed on heroku!
 const db = mongoose.connection;
+
 mongoose.connect(process.env.DATABASE_URL || devConnectionString, { useNewUrlParser: true });
 
-db.on("connected", function(){
-    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-});
-// we will use our machine as the database for now, but make sure
-// that you delete lines 7-11 once you're deployed online and 
-// un-comment lines 2 and 18-20
-
-
 // database connection event
-// mongoose.connection.on('connected', function () {
-//     console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
-// });
+db.on("connected", function(){
+    console.log(`Connected to to: ${process.env.DATABASE_URL} at ${db.host}:${db.port}`);
+});
 
 module.exports = mongoose;
