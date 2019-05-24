b999ce9d5064ce4526f4eaf632f855faec0c7ce9
Albert Chung <chung972@gmail.com>
2019-05-24 09:09:11 -0500

    adds todo comments

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+// remember, with users, we'll only really ever need to
+// SHOW one user at a time; in this case, the action will 
+// essentially be a "my account" page; another thing to keep in mind
+// is that SHOW will RENDER (we won't be changing any data)
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
