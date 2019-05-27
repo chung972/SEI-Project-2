bd9e721977eb448c8bb2b938427715706c77dead
Albert Chung <chung972@gmail.com>
2019-05-27 09:14:16 -0500

    adds logic for index function

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -6,9 +6,16 @@ module.exports = {
 }
 
 function index(req, res){
-
+    Recipe.find({}, (err, recipes) => {
+        res.render("recipes/index", {
+            title: "Recipe List",
+            recipes,
+            user: req.user
+        })
+    })
 }
 
 function show(req, res){
-    
+    Recipe.findById(req.params.id);
+
 }
