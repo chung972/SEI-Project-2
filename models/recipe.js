77bf30a10bfdfc2d9e0547d1871c70154773f228
Albert Chung <chung972@gmail.com>
2019-05-27 08:16:25 -0500

    adds schema for recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -3,3 +3,27 @@
 // reference to a related data entity); this is because both a recipe and comment
 // must know who created; however, a recipe and comment will not necessarily be
 // created by the SAME user (in fact, they most likely won't)
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// two different ways of using Schema; you can assign a Schema const up here,
+// or you can call mongoose.Schema in "var nameSchema = new mongoose.Schema"
+
+var recipeSchema = new Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    ingredients: [{
+        type: String
+    }],
+    imageURL: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Recipe", recipeSchema);
