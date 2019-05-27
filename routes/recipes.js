35a8b01b5016d275149b1394cd9839c9bbcc730c
Albert Chung <chung972@gmail.com>
2019-05-27 08:14:43 -0500

    adds show and index routes for recipes

---
+++ routes/recipes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const recipesCtrl = require("../controllers/recipes");
+
+router.get("/", recipesCtrl.index);
+router.get("/:id", recipesCtrl.show);
+
+module.exports = router;
