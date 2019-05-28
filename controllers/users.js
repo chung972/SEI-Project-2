2d0803bf2a878e1c4713c69782fc5dc40a8b0306
Albert Chung <chung972@gmail.com>
2019-05-28 15:21:52 -0500

    nests another query so we can pass user AND recipes into the context object

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,18 @@
 const User = require("../models/user");
+const Recipe = require("../models/recipe");
 
 module.exports = {
     show
 }
 
-function show(req, res){
+function show(req, res) {
     User.findById(req.params.id, (err, user) => {
-        res.render("users/show", {
-            title: "My Account",
-            user
+        Recipe.find({ user: user._id }, (err, recipes) => {
+            res.render("users/show", {
+                title: "My Account",
+                user,
+                recipes
+            });
         });
     });
 }
