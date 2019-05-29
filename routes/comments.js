90def70729fe9ae6332704d907a7c189dd699636
Albert Chung <chung972@gmail.com>
2019-05-29 11:35:55 -0500

    adds route for deleting comments

--- routes/comments.js
+++ routes/comments.js
@@ -4,10 +4,13 @@ const commentsCtrl = require("../controllers/comments");
 const recipesCtrl = require("../controllers/recipes");
 
 router.post("/recipes/:id/comments", isLoggedIn, commentsCtrl.create);
-router.get("/recipes/:id/comments", isLoggedIn, recipesCtrl.show);
+// router.get("/recipes/:id/comments", isLoggedIn, recipesCtrl.show);
 // we have this .get above to catch the path req.path/req.originalUrl throws;
 // unfortunately, for some reason when we DO catch that get request, that request
 // gets called on repeatedly
+
+router.delete("/recipes/:recipeid/comments/:commentid", isLoggedIn, commentsCtrl.delete);
+// router.delete("/comments/:id", isLoggedIn, commentsCtrl.delete);
 
 module.exports = router;
 
