78180065b2927f6b28e8f062bdf5cd59ef12eb67
Albert Chung <chung972@gmail.com>
2019-05-29 09:10:09 -0500

    adds some test code for catching req paths for isLoggedIn

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const commentsCtrl = require("../controllers/comments");
+const recipesCtrl = require("../controllers/recipes");
 
-router.post("/recipes/:id/comments", commentsCtrl.create);
+router.post("/recipes/:id/comments", isLoggedIn, commentsCtrl.create);
+router.get("/recipes/:id/comments", isLoggedIn, recipesCtrl.show);
+// we have this .get above to catch the path req.path/req.originalUrl throws;
+// unfortunately, for some reason when we DO catch that get request, that request
+// gets called on repeatedly
 
 module.exports = router;
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) return next();
+    console.log(req.path);
+    console.log(req.originalUrl);
+    res.redirect("/");
+}
