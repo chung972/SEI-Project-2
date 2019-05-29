e5eb7c4b9fb02e0a1fd54e532ba98925e5dddab1
Albert Chung <chung972@gmail.com>
2019-05-29 15:02:32 -0500

    adds route for redirecting (in the non-technical sense) to a random recipe

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,12 +6,14 @@ const recipesCtrl = require("../controllers/recipes");
 // different set of RESTful routes: https://gist.github.com/jim-clark/d7e5c9130c9b46b253d4c47aa601596a
 
 router.get("/recipes", recipesCtrl.index);
+router.get("/recipes/random", recipesCtrl.random);  // this is UNrestful, right?
 router.get("/recipes/:id", recipesCtrl.show);
 router.get("/users/:id/recipes/new", isLoggedIn, recipesCtrl.new);
 router.post("/users/:id/recipes", isLoggedIn, recipesCtrl.create);
 router.get("/recipes/:id/edit", isLoggedIn, recipesCtrl.edit);
 router.put("/recipes/:id", isLoggedIn, recipesCtrl.update);
 router.delete("/users/:userid/recipes/:recipeid", isLoggedIn, recipesCtrl.delete);
+
 
 module.exports = router;
 
