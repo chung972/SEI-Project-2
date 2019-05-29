c39394122f3ec3a25ee7d01a9bd5b24072f7b408
Albert Chung <chung972@gmail.com>
2019-05-29 09:07:49 -0500

    adds logic to assign user property to comments in the create function

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,8 +6,20 @@ module. exports = {
 
 function create(req, res){
     Recipe.findById(req.params.id, (err, recipe) => {
+
+        req.body.user = req.user._id;
+        
+        // we want to assign the .user property key in the comment schema to the value of
+        // a user('s)._id; however, in this case, the req.params that's being passed in is
+        // the id for the RECIPE and not the user
+
+        
+        // const comment = { text: 'testing123', user: '5ce830b976e61712f3cec85a' }
         recipe.comments.push(req.body);
         recipe.save(err => {
+            // side note, because we have to save the MODEL whenever we want to add a comment,
+            // we are incrementing the version number (something that we wouldn't normally be
+            // able to do if we just updated via findByIdAndUpdate)
             res.redirect(`/recipes/${recipe._id}`);
         });
     });
